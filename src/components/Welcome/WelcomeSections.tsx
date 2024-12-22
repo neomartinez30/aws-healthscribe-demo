cb3d9eb189a4fd5f82f8792854a79120bae1f95f
neomartinez30 <neo.martinez30@gmail.com>
2024-12-22 11:57:28 -0500

    Update WelcomeSections.tsx

--- src/components/Welcome/WelcomeSections.tsx
+++ src/components/Welcome/WelcomeSections.tsx
@@ -12,15 +12,21 @@ import TextContent from '@cloudscape-design/components/text-content';
 export function Overview() {
     return (
         <SpaceBetween size={'s'}>
-            <Header variant="h2">Overview</Header>
+            <Header variant="h2">
+                <span>Patient Insights</span>
+            </Header>
             <Container>
                 <SpaceBetween size={'s'}>
-                    <TextContent>
-                        <Box variant="p">
-                            This sample web app shows the art of the possible in using AI and GenAI techniques for Next-Gen Virtual Nurse workspaces.
-                            <li>This demo website uses synthetic patient data. Nothing is real.</li>
-                        </Box>
-                    </TextContent>
+                    <Box>
+                        The patient insights module is powered by AWS HealthLake and AWS Berock.
+                        <ul>
+                            <li>Database Integration: Connect databased to Amazon HealthLake and select the resources to analyze.</li>
+                            <li>Summarization: Generate detailed summaries of patient medical data by leveraging Amazon Bedrock's Generative AI capabilities.</li>
+                            <li>Consolidated Summary: Obtain a comprehensive, coherent summary of the patient's medical history by merging information from various sources.</li>
+                            <li>Individual FHIR summary: Obtain a more granular summary of each FHIR resource selected for analysis.</li>
+                            <li>Interactive Chat: Engage in a conversational interface to ask questions about the patient's medical record and receive relevant answers.</li>
+                        </ul>
+                    </Box>
                 </SpaceBetween>
             </Container>
         </SpaceBetween>
@@ -29,12 +35,12 @@ export function Overview() {
 
 export function Highlights() {
     return (
-        <SpaceBetween size={'s'}>
+        <SpaceBetween size="s">
             <Header variant="h2">Conversations</Header>
             <Container>
-                <SpaceBetween size={'s'}>
+                <SpaceBetween size="s">
                     <Box>
-                        The <b>conversations<b> tab is powered by AWS Healthscribe and AWS Bedrock. AWS HealthScribe is a HIPAA-eligible service empowering healthcare software vendors to build clinical applications that automatically generate clinical notes by analyzing patient-clinician conversations.
+                        The <b>conversations</b> tab is powered by AWS HealthScribe and AWS Bedrock. AWS HealthScribe is a HIPAA-eligible service empowering healthcare software vendors to build clinical applications that automatically generate clinical notes by analyzing patient-clinician conversations.
                         <ul>
                             <li>Summarized clinical notes</li>
                             <li>Rich consultation transcripts</li>
@@ -74,108 +80,7 @@ export function Details() {
 }
 
 
-import React from 'react';
-import Box from '@cloudscape-design/components/box';
-import Container from '@cloudscape-design/components/container';
-import Header from '@cloudscape-design/components/header';
-import Link from '@cloudscape-design/components/link';
-import SpaceBetween from '@cloudscape-design/components/space-between';
-import TextContent from '@cloudscape-design/components/text-content';
 
-export function Overview() {
-    return (
-        <SpaceBetween size="s">
-            <Header variant="h2">Overview</Header>
-            <Container>
-                <SpaceBetween size="s">
-                    <TextContent>
-                        <Box variant="p">
-                            This sample web app shows the art of the possible in using AI and GenAI techniques for Next-Gen Virtual Nurse workspaces.
-                            <ul>
-                                <li>This demo website uses synthetic patient data. Nothing is real.</li>
-                            </ul>
-                        </Box>
-                    </TextContent>
-                </SpaceBetween>
-            </Container>
-        </SpaceBetween>
-    );
-}
-
-export function Highlights() {
-    return (
-        <SpaceBetween size="s">
-            <Header variant="h2">Conversations</Header>
-            <Container>
-                <SpaceBetween size="s">
-                    <Box>
-                        The <b>conversations</b> tab is powered by AWS HealthScribe and AWS Bedrock. AWS HealthScribe is a HIPAA-eligible service empowering healthcare software vendors to build clinical applications that automatically generate clinical notes by analyzing patient-clinician conversations.
-                        <ul>
-                            <li>Summarized clinical notes</li>
-                            <li>Rich consultation transcripts</li>
-                            <li>Transcript segmentation</li>
-                            <li>Evidence mapping</li>
-                            <li>Structured medical terms</li>
-                        </ul>
-                    </Box>
-                </SpaceBetween>
-            </Container>
-        </SpaceBetween>
-    );
-}
-
-export function Details() {
-    return (
-        <SpaceBetween size="s">
-            <Header variant="h2">
-                <span>Patient Insights</span>
-            </Header>
-            <Container>
-                <SpaceBetween size="s">
-                    <Box>
-                        The patient insights module is powered by AWS HealthLake and AWS Bedrock.
-                        <ul>
-                            <li>Database Integration: Connect databases to Amazon HealthLake and select the resources to analyze.</li>
-                            <li>Summarization: Generate detailed summaries of patient medical data by leveraging Amazon Bedrock's Generative AI capabilities.</li>
-                            <li>Consolidated Summary: Obtain a comprehensive, coherent summary of the patient's medical history by merging information from various sources.</li>
-                            <li>Individual FHIR summary: Obtain a more granular summary of each FHIR resource selected for analysis.</li>
-                            <li>Interactive Chat: Engage in a conversational interface to ask questions about the patient's medical record and receive relevant answers.</li>
-                        </ul>
-                    </Box>
-                </SpaceBetween>
-            </Container>
-        </SpaceBetween>
-    );
-}
-
-
-import React from 'react';
-import Box from '@cloudscape-design/components/box';
-import Container from '@cloudscape-design/components/container';
-import Header from '@cloudscape-design/components/header';
-import Link from '@cloudscape-design/components/link';
-import SpaceBetween from '@cloudscape-design/components/space-between';
-import TextContent from '@cloudscape-design/components/text-content';
-
-export function Overview() {
-    return (
-        <SpaceBetween size="s">
-            <Header variant="h2">Overview</Header>
-            <Container>
-                <SpaceBetween size="s">
-                    <TextContent>
-                        <Box variant="p">
-                            This sample web app shows the art of the possible in using AI and GenAI techniques for Next-Gen Virtual Nurse workspaces.
-                            <ul>
-                                <li>This demo website uses synthetic patient data. Nothing is real.</li>
-                            </ul>
-                        </Box>
-                    </TextContent>
-                </SpaceBetween>
-            </Container>
-        </SpaceBetween>
-    );
-}
 
 export function Highlights() {
     return (
