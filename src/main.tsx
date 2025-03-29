9352a7702f203c717d284f97787ec129d309a5fc
neomartinez30 <neo.martinez30@gmail.com>
2025-03-29 11:33:27 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,14 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Box from '@cloudscape-design/components/box';
 import { I18nProvider, importMessages } from '@cloudscape-design/components/i18n';
 import '@cloudscape-design/global-styles/index.css';
-import './index.css'; // Import global styles
-import { Authenticator } from '@aws-amplify/ui-react';
+import './index.css';
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
+import Box from '@cloudscape-design/components/box';
 
 import AppSettingsContextProvider from '@/store/appSettings';
 import AppThemeContextProvider from '@/store/appTheme';
-import AuthContextProvider from '@/store/auth';
 import NotificationsContextProvider from '@/store/notifications';
 
 import { App } from './components';
@@ -25,20 +21,16 @@ root.render(
     <React.StrictMode>
         <BrowserRouter>
             <I18nProvider locale={locale} messages={messages}>
-                <Authenticator.Provider>
-                    <AuthContextProvider>
-                        <AppThemeContextProvider>
-                            <AppSettingsContextProvider>
-                                <NotificationsContextProvider>
-                                    <App />
-                                    <Box>
-                                        <Toaster position="bottom-left" reverseOrder={false} />
-                                    </Box>
-                                </NotificationsContextProvider>
-                            </AppSettingsContextProvider>
-                        </AppThemeContextProvider>
-                    </AuthContextProvider>
-                </Authenticator.Provider>
+                <AppThemeContextProvider>
+                    <AppSettingsContextProvider>
+                        <NotificationsContextProvider>
+                            <App />
+                            <Box>
+                                <Toaster position="bottom-left" reverseOrder={false} />
+                            </Box>
+                        </NotificationsContextProvider>
+                    </AppSettingsContextProvider>
+                </AppThemeContextProvider>
             </I18nProvider>
         </BrowserRouter>
     </React.StrictMode>
