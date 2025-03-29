2d83f52ed26df92a89cbe83aefd5bb41e23dc261
neomartinez30 <neo.martinez30@gmail.com>
2025-03-29 12:36:02 -0400

    Update TopNav.tsx

--- src/components/TopNav/TopNav.tsx
+++ src/components/TopNav/TopNav.tsx
@@ -1,15 +1,9 @@
-import React, { Suspense, lazy, useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
 import TopNavigation from '@cloudscape-design/components/top-navigation';
 import { TopNavigationProps } from '@cloudscape-design/components/top-navigation';
 import { Density, Mode, applyDensity, applyMode } from '@cloudscape-design/global-styles';
-
-import Auth from '@/components/Auth';
-import ModalLoader from '@/components/SuspenseLoader/ModalLoader';
 import { useAppThemeContext } from '@/store/appTheme';
-import { useAuthContext } from '@/store/auth';
-
 import './TopNav.css';
 
 interface TopNavClick {
@@ -20,30 +14,7 @@ interface TopNavClick {
 
 export default function TopNav() {
     const navigate = useNavigate();
-    const { isUserAuthenticated, user, signOut } = useAuthContext();
     const { appTheme, setAppThemeColor, setAppThemeDensity } = useAppThemeContext();
-    const [authVisible, setAuthVisible] = useState(false);
-
-    // Set app theme
-    useEffect(() => {
-        if (appTheme.color === 'appTheme.light') {
-            applyMode(Mode.Light);
-        } else if (appTheme.color === 'appTheme.dark') {
-            applyMode(Mode.Dark);
-        }
-
-        if (appTheme.density === 'density.comfortable') {
-            applyDensity(Density.Comfortable);
-        } else if (appTheme.density === 'density.compact') {
-            applyDensity(Density.Compact);
-        }
-    }, [appTheme]);
-
-    useEffect(() => {
-        if (isUserAuthenticated) {
-            setAuthVisible(false);
-        }
-    }, [isUserAuthenticated]);
 
     function handleUtilVisualClick(e: TopNavClick) {
         switch (e.detail.id) {
@@ -148,56 +119,25 @@ export default function TopNav() {
         ]
     };
 
-    const utilUser: TopNavigationProps.Utility = isUserAuthenticated
-        ? {
-              type: 'menu-dropdown',
-              text: user?.signInDetails?.loginId || user?.username,
-              description: user?.signInDetails?.loginId,
-              iconName: 'user-profile',
-              items: [
-                  { id: 'profile', text: 'Your Profile' },
-                  { id: 'preferences', text: 'Preferences' },
-                  { id: 'divider-1', text: '-' },
-                  { id: 'signout', text: 'Sign out' }
-              ],
-              onItemClick: (e) => {
-                  if (e.detail.id === 'signout') {
-                      signOut();
-                  }
-              }
-          }
-        : {
-              type: 'button',
-              text: 'Sign In',
-              onClick: () => setAuthVisible(true),
-          };
-
     return (
-        <>
-            {authVisible && (
-                <Suspense fallback={<ModalLoader />}>
-                    <Auth setVisible={setAuthVisible} />
-                </Suspense>
-            )}
-            <TopNavigation
-                identity={{
-                    href: '/',
-                    title: 'Virtual Nurse Workspace',
-                    logo: {
-                        src: "https://www.tricare.mil/About/-/media/69C0BE06D96345CC872565890A0CC4B1.ashx",
-                        alt: "DHA Logo"
-                    }
-                }}
-                utilities={[utilHelp, utilVisual, utilUser]}
-                i18nStrings={{
-                    searchIconAriaLabel: 'Search',
-                    searchDismissIconAriaLabel: 'Close search',
-                    overflowMenuTriggerText: 'More',
-                    overflowMenuTitleText: 'All',
-                    overflowMenuBackIconAriaLabel: 'Back',
-                    overflowMenuDismissIconAriaLabel: 'Close menu'
-                }}
-            />
-        </>
+        <TopNavigation
+            identity={{
+                href: '/',
+                title: 'Virtual Nurse Workspace',
+                logo: {
+                    src: "https://www.tricare.mil/About/-/media/69C0BE06D96345CC872565890A0CC4B1.ashx",
+                    alt: "DHA Logo"
+                }
+            }}
+            utilities={[utilHelp, utilVisual]}
+            i18nStrings={{
+                searchIconAriaLabel: 'Search',
+                searchDismissIconAriaLabel: 'Close search',
+                overflowMenuTriggerText: 'More',
+                overflowMenuTitleText: 'All',
+                overflowMenuBackIconAriaLabel: 'Back',
+                overflowMenuDismissIconAriaLabel: 'Close menu'
+            }}
+        />
     );
 }
