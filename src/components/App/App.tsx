172e910a8f103fa37076cb48c69d149d7c99a7e0
neomartinez30 <neo.martinez30@gmail.com>
2025-03-29 11:31:39 -0400

    Update App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,7 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
 import React, { Suspense, lazy } from 'react';
-
 import { Navigate, Route, Routes } from 'react-router-dom';
-
 import AppLayout from '@cloudscape-design/components/app-layout';
-import Flashbar from '@cloudscape-design/components/flashbar';
-
-import Breadcrumbs from '@/components/Breadcrumbs';
-import SideNav from '@/components/SideNav';
 import SuspenseLoader from '@/components/SuspenseLoader';
-import TopNav from '@/components/TopNav';
-import Welcome from '@/components/Welcome';
-import { useAuthContext } from '@/store/auth';
-import { useNotificationsContext } from '@/store/notifications';
 
 // Lazy components
 const Debug = lazy(() => import('@/components/Debug'));
@@ -22,46 +10,36 @@ const Conversations = lazy(() => import('@/components/Conversations'));
 const Conversation = lazy(() => import('@/components/Conversation'));
 const NewConversation = lazy(() => import('@/components/NewConversation'));
 const GenerateAudio = lazy(() => import('@/components/GenerateAudio'));
+const PatientInsights = lazy(() => import('@/components/PatientInsights'));
+const AgentDesktop = lazy(() => import('@/components/AgentDesktop'));
 
 export default function App() {
-    const { isUserAuthenticated } = useAuthContext();
-    const { flashbarItems } = useNotificationsContext();
-
     const content = (
         <Suspense fallback={<SuspenseLoader />}>
-            {isUserAuthenticated ? (
-                <Routes>
-                    <Route index element={<Welcome />} />
-                    <Route path="/debug" element={<Debug />} />
-                    <Route path="/conversations" element={<Conversations />} />
-                    <Route path="/conversation/:conversationName" element={<Conversation />} />
-                    <Route path="/new" element={<NewConversation />} />
-                    <Route path="/generate" element={<GenerateAudio />} />
-                    <Route path="/settings" element={<Settings />} />
-                    <Route path="*" element={<Navigate to="/" replace />} />
-                </Routes>
-            ) : (
-                <Routes>
-                    <Route path="*" element={<Welcome />} />
-                </Routes>
-            )}
+            <Routes>
+                <Route path="/" element={<AgentDesktop />} />
+                <Route path="/debug" element={<Debug />} />
+                <Route path="/conversations" element={<Conversations />} />
+                <Route path="/conversation/:conversationName" element={<Conversation />} />
+                <Route path="/new" element={<NewConversation />} />
+                <Route path="/generate" element={<GenerateAudio />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/PatientInsights" element={<PatientInsights />} />
+                <Route path="/AgentDesktop" element={<AgentDesktop/>} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
         </Suspense>
     );
 
     return (
-        <>
-            <div id="appTopNav">
-                <TopNav />
-            </div>
-            <AppLayout
-                breadcrumbs={<Breadcrumbs />}
-                content={content}
-                headerSelector="#appTopNav"
-                headerVariant="high-contrast"
-                navigation={<SideNav />}
-                notifications={<Flashbar items={flashbarItems} />}
-                toolsHide={true}
-            />
-        </>
+        <AppLayout
+            content={content}
+            headerSelector="#appTopNav"
+            contentType="default"
+            disableContentPaddings={false}
+            navigationHide={true}
+            toolsHide={true}
+            maxContentWidth={1280}
+        />
     );
 }
