b8bb5c3b4dbd9c6fdcd9f2fa6067119fcbefb1bb
neomartinez30 <neo.martinez30@gmail.com>
2025-03-30 17:12:18 -0400

    Update Welcome.tsx

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -4,7 +4,7 @@ import SpaceBetween from '@cloudscape-design/components/space-between';
 import WelcomeHeader from './WelcomeHeader';
 import { Details, Highlights, Overview } from './WelcomeSections';
 
-const logoUrl = "https://www.tricare.mil/About/-/media/69C0BE06D96345CC872565890A0CC4B1.ashx";
+const logoUrl = "https://health.mil/-/media/Images/MHS/Infographics/DHA-Logo.svg?h=60&iar=0&w=138&hash=5890E798CDC400D0742649D53B4BD1694BC89A7F";
 
 function Welcome() {
     return (
